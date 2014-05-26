72365873d51ca98da779b911a3f2f354cc607836
Simon de Haan <simon@praekeltfoundation.org>
2014-05-26 06:42:40 +0200

    pep8 gamification

--- gamification/models.py
+++ gamification/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from organisation.models import Course, Module
 
 
-# BadgeTemplates can be specified and linked to a Course and Scenario. A BadgeTemplate has a name, an image (jpg, png,
-# gif) and a description. Badges are instances of a BadgeTemplate awarded to a specific user.
+# BadgeTemplates can be specified and linked to a Course and Scenario.
+# A BadgeTemplate has a name, an image (jpg, png, gif) and a description.
+# Badges are instances of a BadgeTemplate awarded to a specific user.
 class GamificationPointBonus(models.Model):
-    name = models.CharField("Name", max_length=50, null=True, blank=False, unique=True)
+    name = models.CharField(
+        "Name", max_length=50, null=True, blank=False, unique=True)
     description = models.CharField("Description", max_length=50, blank=True)
     image = models.ImageField("Image", upload_to="img/", blank=True, null=True)
     value = models.PositiveIntegerField("Value", null=True, blank=False)
@@ -18,10 +20,13 @@ class GamificationPointBonus(models.Model):
         verbose_name_plural = "Point Bonuses"
 
 
-# PointBonuses are also linked to a Course and Scenario and award the user extra points for achieving a specific
-# scenario. A PointBonus has a name, an image (jpg, png, gif), a description and the number of points to be awarding.
+# PointBonuses are also linked to a Course and Scenario and award the
+# user extra points for achieving a specific scenario. A PointBonus has
+# a name, an image (jpg, png, gif), a description and the number of
+# points to be awarding.
 class GamificationBadgeTemplate(models.Model):
-    name = models.CharField("Name", max_length=50, null=True, blank=False, unique=True)
+    name = models.CharField(
+        "Name", max_length=50, null=True, blank=False, unique=True)
     description = models.CharField("Description", max_length=50, blank=True)
     image = models.ImageField("Image", upload_to="img/", blank=True, null=True)
 
@@ -29,7 +34,8 @@ class GamificationBadgeTemplate(models.Model):
         return self.name
 
     def image_(self):
-        return '<a href="/media/{0}"><img src="/media/{0}"></a>'.format(self.image)
+        return '<a href="/media/{0}"><img src="/media/{0}"></a>'.format(
+            self.image)
     image_.allow_tags = True
 
     class Meta:
@@ -37,10 +43,12 @@ class GamificationBadgeTemplate(models.Model):
         verbose_name_plural = "Badge Templates"
 
 
-# Gamification is one of the hardest problems to solve elegantly on this platform. We want to be able to hand out extra
-# points and badges based on specific conditions (scenarios) being met by the user.
+# Gamification is one of the hardest problems to solve elegantly on this
+# platform. We want to be able to hand out extra points and badges based
+# on specific conditions (scenarios) being met by the user.
 class GamificationScenario(models.Model):
-    name = models.CharField("Name", max_length=50, null=True, blank=False, unique=True)
+    name = models.CharField(
+        "Name", max_length=50, null=True, blank=False, unique=True)
     description = models.CharField("Description", max_length=50, blank=True)
     event = models.CharField("Event", max_length=50, blank=True)
     course = models.ForeignKey(Course, null=True, blank=False)
