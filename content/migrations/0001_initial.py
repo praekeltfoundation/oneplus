6655476dab19c47d3a5a9680192d648c68b35c95
Theo Crous <tcrous@retrorabbit.co.za>
2014-05-27 16:14:06 +0200

    Added South migration dependencies

--- content/migrations/0001_initial.py
+++ content/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("organisation", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'LearningChapter'
         db.create_table(u'content_learningchapter', (
