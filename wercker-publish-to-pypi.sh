613a6cc8281414717ab558a7595739ca7bcf048b
Simon de Haan <simon@praekeltfoundation.org>
2014-05-29 06:02:56 +0200

    bump version number and fix wercker deploy env variables

--- wercker-publish-to-pypi.sh
+++ wercker-publish-to-pypi.sh
@@ -1,4 +1,4 @@
 echo "[server-login]" > ~/.pypirc
-echo "username:" $PRAEKELT_PYPI_USER >> ~/.pypirc
-echo "password:" $PRAEKELT_PYPI_PASSWORD >> ~/.pypirc
+echo "username:" $PYPI_USER >> ~/.pypirc
+echo "password:" $PYPI_PASSWORD >> ~/.pypirc
 python setup.py sdist upload
