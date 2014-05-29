b45c3f08dd737c008170ab95c27277334b86e73c
Theo Crous <tcrous@retrorabbit.co.za>
2014-05-29 10:35:22 +0200

    static files issue

--- oneplusmvp/settings.py
+++ oneplusmvp/settings.py
@@ -97,8 +97,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 PROJECT_PATH = BASE_DIR
-STATIC_ROOT = "staticfiles"
-STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_PATH, 'static'),
