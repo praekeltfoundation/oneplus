bbb3cfc246e9a068d8eb22dd6d393145052a3ea1
Simon de Haan <simon@praekeltfoundation.org>
2014-06-03 17:38:59 +0200

    publish pip & commit log

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,3 +2,5 @@ manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
 
 $manage syncdb --noinput --no-initial-data --migrate
 $manage collectstatic --noinput
+${PIP} freeze > oneplusmvp/static/pip-freeze.txt
+git log -50 --pretty=format:'%ai - %h %s (%cr) <%an>' > oneplusmvp/static/git-commits.log
