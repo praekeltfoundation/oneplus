20288f315466e76ee7be18c053f4216d96d1ec43
jaderabbit <jabbott@retrorabbit.co.za>
2014-06-19 15:43:31 +0200

    Updated version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-mobileu',
-    version='1.0.9',
+    version='1.0.10',
     packages=find_packages(),
     install_requires=[
         'Django==1.6.5',
