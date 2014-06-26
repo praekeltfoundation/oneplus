2a28c304ea3cc7988799da694bc39d3a0f93f6f0
jaderabbit <jabbott@retrorabbit.co.za>
2014-06-26 10:48:09 +0200

    Updated dependancies of initial migration

--- oneplus/migrations/0002_load_initial_data.py
+++ oneplus/migrations/0002_load_initial_data.py
@@ -15,6 +15,7 @@ class Migration(DataMigration):
         ("core", "0005_auto__add_field_participant_points"),
         ("auth", "0002_auto__add_unique_customuser_mobile"),
         ("auth", "0003_auto__add_field_customuser_unique_token__add_field_customuser_unique_t"),
+        ("communication", "0011_auto__add_sms"),
         ("auth", "0004_auto__chg_field_customuser_unique_token__add_field_learner_last_maths_"),
         ("oneplus", "0001_initial"),
 
