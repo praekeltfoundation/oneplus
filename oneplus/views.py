b05938b9d8bad83589960ecbe48c0ea74e957fc7
retrorabbitWD <wdawson@retrorabbit.co.za>
2014-06-27 13:29:32 +0200

    syntax for 2.6

--- oneplus/views.py
+++ oneplus/views.py
@@ -62,7 +62,7 @@ def oneplus_state_required(f):
 # Action resolver to elegantly handle verbs in the views
 def resolve_http_method(request, methods):
     if isinstance(methods, list):
-        methods = {func.__name__.lower(): func for func in methods}
+        methods = dict([(func.__name__.lower(), func) for func in methods])
     if request.method.lower() not in methods.keys():
         return HttpResponse(status=501)
     return methods[request.method.lower()]()
