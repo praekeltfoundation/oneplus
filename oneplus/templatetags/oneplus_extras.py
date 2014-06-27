de3cc5f19e263fe49946c41576acdaa59dc1165b
retrorabbitWD <wdawson@retrorabbit.co.za>
2014-06-27 16:20:27 +0200

    make width 100%

--- oneplus/templatetags/oneplus_extras.py
+++ oneplus/templatetags/oneplus_extras.py
@@ -6,7 +6,7 @@ register = template.Library()
 def format_width(value):
     soup = BeautifulSoup(value)
     tag = soup.img
-    tag['style'] = 'width:50%'
+    tag['style'] = 'width:100%'
     return tag
 
 register.filter('format_width', format_width)
