378d9b6cb82b5f215057d475f21230655f9bff05
jaderabbit <jabbott@retrorabbit.co.za>
2014-06-29 16:12:35 +0200

    Added in export for test questions

--- content/admin.py
+++ content/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from models import TestingBank, TestingQuestion, TestingQuestionOption, LearningChapter
-
-
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from import_export import fields
+from core.models import ParticipantQuestionAnswer
 class TestingQuestionInline(admin.TabularInline):
     model = TestingQuestion
     extra = 1
@@ -44,10 +46,93 @@ class TestingBankAdmin(SummernoteModelAdmin):
     ordering = ("module", "order", "name", )
 
 
-class TestingQuestionAdmin(SummernoteModelAdmin):
-    list_display = ("bank", "order", "name", "description")
+class TestingQuestionResource(resources.ModelResource):
+
+    class Meta:
+        model = TestingQuestion
+        fields = (
+            'id',
+            'name',
+            'description',
+            'bank',
+            'percentage_correct',
+            'correct',
+            'incorrect'
+        )
+        export_order = (
+            'id',
+            'name',
+            'description',
+            'bank',
+            'percentage_correct',
+            'correct',
+            'incorrect'
+        )
+
+    correct = fields.Field(column_name=u'correct')
+    incorrect = fields.Field(column_name=u'incorrect')
+    percentage_correct = fields.Field(column_name=u'percentage_correct')
+
+
+    def dehydrate_correct(self, question):
+        return ParticipantQuestionAnswer.objects.filter(
+            question=question,
+            correct=True
+        ).count()
+
+
+    def dehydrate_incorrect(self, question):
+        return ParticipantQuestionAnswer.objects.filter(
+            question=question,
+            correct=False
+        ).count()
+
+
+    def dehydrate_percentage_correct(self, question):
+        correct = ParticipantQuestionAnswer.objects.filter(
+            question=question,
+            correct=True
+        ).count()
+        total = ParticipantQuestionAnswer.objects.filter(
+            question=question
+        ).count()
+
+        return 100*correct/total
+
+class TestingQuestionAdmin(SummernoteModelAdmin, ImportExportModelAdmin):
+    list_display = ("bank", "order", "name", "description",
+                    "correct", "incorrect", "percentage_correct")
     list_filter = ("bank", )
     search_fields = ("name", "description")
+
+    def correct(self, question):
+        return ParticipantQuestionAnswer.objects.filter(
+            question=question,
+            correct=True
+        ).count()
+    correct.allow_tags = True
+    correct.short_description = "Correct"
+
+    def incorrect(self, question):
+        return ParticipantQuestionAnswer.objects.filter(
+            question=question,
+            correct=False
+        ).count()
+    incorrect.allow_tags = True
+    incorrect.short_description = "Incorrect"
+
+    def percentage_correct(self, question):
+        correct = ParticipantQuestionAnswer.objects.filter(
+            question=question,
+            correct=True
+        ).count()
+        total = ParticipantQuestionAnswer.objects.filter(
+            question=question
+        ).count()
+        return 100*correct/total
+    percentage_correct.allow_tags = True
+    percentage_correct.short_description = "Percentage Correct"
+
     fieldsets = [
         (None,
             {"fields": ["name", "description", "bank", "order"]}),
@@ -56,7 +141,8 @@ class TestingQuestionAdmin(SummernoteModelAdmin):
                         "difficulty", "points"]})
     ]
     inlines = (TestingQuestionOptionInline,)
-    ordering = ("bank", "order", "name", )
+    resource_class = TestingQuestionResource
+
 
 
 class TestingQuestionOptionAdmin(SummernoteModelAdmin):
