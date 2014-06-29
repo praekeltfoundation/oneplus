b9218fb58782e1b3f5cadcb345729358f72d1aab
retrorabbitWD <wdawson@retrorabbit.co.za>
2014-06-29 15:22:45 +0200

    test is None on method

--- oneplus/templatetags/oneplus_extras.py
+++ oneplus/templatetags/oneplus_extras.py
@@ -6,7 +6,8 @@ register = template.Library()
 def format_width(value):
     soup = BeautifulSoup(value)
     tag = soup.img
-    tag['style'] = 'width:100%'
-    return tag
+    if tag is not None:
+        tag['style'] = 'width:100%'
+    return soup
 
 register.filter('format_width', format_width)
