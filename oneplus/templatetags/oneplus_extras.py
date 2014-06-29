47479d92676ba5a73b302fed9a782a7ae67c4c43
jaderabbit <jabbott@retrorabbit.co.za>
2014-06-29 19:32:10 +0200

    Returns full html and checks if img exists

--- oneplus/templatetags/oneplus_extras.py
+++ oneplus/templatetags/oneplus_extras.py
@@ -5,8 +5,8 @@ register = template.Library()
 
 def format_width(value):
     soup = BeautifulSoup(value)
-    tag = soup.img
-    tag['style'] = 'width:100%'
-    return tag
+    if soup.img is not None:
+        soup.img['style'] = 'width:100%'
+    return soup
 
 register.filter('format_width', format_width)
