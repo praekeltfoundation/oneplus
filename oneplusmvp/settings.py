f3c52971c23a56093008ccc0d938a72767614ec1
retrorabbitWD <wdawson@retrorabbit.co.za>
2014-07-10 15:36:03 +0200

    added sites to install apps

--- oneplusmvp/settings.py
+++ oneplusmvp/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = (
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
+    "django.contrib.sites",
     "oneplus",
     "core",
     "gamification",
