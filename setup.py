99aa76ef4238aa87b781ee0c300bb9c3078d1146
jaderabbit <jabbott@retrorabbit.co.za>
2014-07-16 15:10:33 +0200

    Updated summernote version to latest (Thanks @retrorabbitWD)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'Django==1.6.5',
         'South==0.8.4',
         'django-grappelli==2.5.3',
-        'django-summernote==0.5.12',
+        'django-summernote==0.5.13',
         'django-import-export==0.2.2',
         'Pillow==2.4.0',
         'psycopg2==2.5.3',
