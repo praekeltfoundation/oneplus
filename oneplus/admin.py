83535cbfb0981cccc8b6e2c22d2b9485ad0c8c08
retrorabbitWD <wdawson@retrorabbit.co.za>
2014-08-04 15:45:28 +0200

    change course to class and update order

--- oneplus/admin.py
+++ oneplus/admin.py
@@ -8,13 +8,40 @@ from organisation.models import School
 
 
 class OnePlusLearnerResource(LearnerResource):
-    course = fields.Field(column_name=u'course')
+    class_name = fields.Field(column_name=u'class')
     completed_questions = fields.Field(column_name=u'completed_questions')
     percentage_correct = fields.Field(column_name=u'percentage_correct')
+
+    class Meta:
+        model = Learner
+        exclude = (
+            'customuser_ptr', 'password', 'last_login', 'is_superuser',
+            'groups', 'user_permissions', 'is_staff', 'is_active',
+            'date_joined', 'unique_token', 'unique_token_expiry'
+            'welcome_message_sent', 'welcome_message'
+        )
+        export_order = (
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'mobile',
+            'school',
+            'country',
+            'area',
+            'city',
+            'optin_sms',
+            'optin_email',
+            'completed_questions',
+            'percentage_correct',
+            'class_name',
+        )
 
     def import_obj(self, obj, data, dry_run):
         school, created = School.objects.get_or_create(name=data[u'school'])
         data[u'school'] = school.id
+        data[u'mobile'] = data[u'username']
         return super(LearnerResource, self)\
             .import_obj(obj, data, dry_run)
 
