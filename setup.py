71941bc2b0307c7fceb34286536e094464a1535a
jaderabbit <jabbott@retrorabbit.co.za>
2014-08-12 09:02:41 +0200

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-mobileu',
-    version='1.0.34',
+    version='1.0.35',
     packages=find_packages(),
     install_requires=[
         'Django==1.6.5',
