6273c30fbfdcd03143567b05e89b28cd292e40ea
retrorabbitWD <wdawson@retrorabbit.co.za>
2014-08-19 13:19:40 +0200

    move right veiw to general tests

--- oneplus/tests.py
+++ oneplus/tests.py
@@ -495,6 +495,26 @@ class GeneralTests(TestCase):
         output = strip_tags(result)
         self.assertEquals(output, u'<div></div>')
 
+    def test_right_view(self):
+        self.client.get(reverse(
+            'auth.autologin',
+            kwargs={'token': self.learner.unique_token})
+        )
+        question = self.create_test_question('question1', self.testbank,
+                                             question_content='test question')
+        questionoption = self.create_test_question_option('questionoption1',
+                                                          question)
+
+        # Post a correct answer
+        self.client.post(
+            reverse('learn.next'),
+            data={'answer': questionoption.id}
+        )
+        point = get_points_awarded(self.participant)
+        badge, badge_points = get_badge_awarded(self.participant)
+        self.assertEqual(point, 5)
+        self.assertEqual(badge, self.badge_template)
+
 
 class LearnerStateTest(TestCase):
     def create_course(self, name="course name", **kwargs):
@@ -628,23 +648,5 @@ class LearnerStateTest(TestCase):
 
         self.assertEquals(active_question.name,'q1')
 
-    def test_right_view(self):
-        self.client.get(reverse(
-            'auth.autologin',
-            kwargs={'token': self.learner.unique_token})
-        )
-        question = self.create_test_question('question1', self.testbank,
-                                             question_content='test question')
-        questionoption = self.create_test_question_option('questionoption1',
-                                                          question)
 
-        # Post a correct answer
-        self.client.post(
-            reverse('learn.next'),
-            data={'answer': questionoption.id}
-        )
-        point = get_points_awarded(self.participant)
-        badge, badge_points = get_badge_awarded(self.participant)
-        self.assertEqual(point, 5)
-        self.assertEqual(badge, self.badge_template)
 
