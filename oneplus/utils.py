f24668dd181724c2bdb044a054f0631f64058da1
retrorabbitWD <wdawson@retrorabbit.co.za>
2014-08-25 17:27:38 +0200

    add logger to utils

--- oneplus/utils.py
+++ oneplus/utils.py
@@ -1,4 +1,4 @@
-from go_http import HttpApiSender
+from go_http import HttpApiSender, LoggingSender
 import oneplusmvp.settings as settings
 from datetime import datetime
 import requests
@@ -6,12 +6,18 @@ import requests
 
 def update_metric(name, value, metric_type):
     try:
-        sender = HttpApiSender(
-            account_key=settings.VUMI_GO_ACCOUNT_KEY,
-            conversation_key=settings.VUMI_GO_CONVERSATION_KEY,
-            conversation_token=settings.VUMI_GO_ACCOUNT_TOKEN
+        if hasattr(settings, 'VUMI_GO_FAKE') and settings.VUMI_GO_FAKE:
+            sender = LoggingSender(
+                'DEBUG'
+            )
+            sender.fire_metric(name, value, agg=metric_type.lower())
+        else:
+            sender = HttpApiSender(
+                account_key=settings.VUMI_GO_ACCOUNT_KEY,
+                conversation_key=settings.VUMI_GO_CONVERSATION_KEY,
+                conversation_token=settings.VUMI_GO_ACCOUNT_TOKEN
         )
-        sender.fire_metric(name, value, agg=metric_type)
+            sender.fire_metric(name, value, agg=metric_type)
     except requests.exceptions.RequestException as e:
         pass
 
