401bf3f5a813ce6e0daf50162784e68c89e7dbbe
Rizmari Versfeld <rizziepit@gmail.com>
2014-10-26 19:25:53 +0200

    put Travis test commands in script file

---
+++ scripts/run-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python manage.py syncdb | tee
+python manage.py migrate
+python manage.py syncdb --all --noinput  # call AFTER migrations for permission creation
+coverage run --source=oneplus manage.py test
