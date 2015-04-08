226eafe91436dd20c91dbf6bacb7f20dc30331e2
msinovcic <msinovcic@retrorabbit.co.za>
2015-04-08 16:20:38 +0200

    trying to fix migration issue

---
+++ content/migrations/0013_auto__add_field_mathml_source.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Mathml.source'
+        db.add_column(u'content_mathml', 'source',
+                      self.gf('django.db.models.fields.IntegerField')(default=0, max_length=1),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Mathml.source'
+        db.delete_column(u'content_mathml', 'source')
+
+
+    models = {
+        u'content.learningchapter': {
+            'Meta': {'object_name': 'LearningChapter'},
+            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'module': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['organisation.Module']", 'null': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '500', 'unique': 'True', 'null': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'})
+        },
+        u'content.mathml': {
+            'Meta': {'object_name': 'Mathml'},
+            'error': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'filename': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mathml_content': ('django.db.models.fields.TextField', [], {}),
+            'rendered': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'source': ('django.db.models.fields.IntegerField', [], {'max_length': '1'}),
+            'source_id': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'content.testingquestion': {
+            'Meta': {'ordering': "['name']", 'object_name': 'TestingQuestion'},
+            'answer_content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            'difficulty': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'module': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['organisation.Module']", 'null': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '500', 'unique': 'True', 'null': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'}),
+            'points': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'}),
+            'question_content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'textbook_link': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'})
+        },
+        u'content.testingquestionoption': {
+            'Meta': {'object_name': 'TestingQuestionOption'},
+            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'correct': ('django.db.models.fields.BooleanField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '500', 'unique': 'True', 'null': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['content.TestingQuestion']", 'null': 'True'})
+        },
+        u'organisation.course': {
+            'Meta': {'object_name': 'Course'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '500', 'unique': 'True', 'null': 'True'}),
+            'question_order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'blank': 'True'})
+        },
+        u'organisation.coursemodulerel': {
+            'Meta': {'object_name': 'CourseModuleRel'},
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['organisation.Course']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'module': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['organisation.Module']"})
+        },
+        u'organisation.module': {
+            'Meta': {'object_name': 'Module'},
+            'courses': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'modules'", 'symmetrical': 'False', 'through': u"orm['organisation.CourseModuleRel']", 'to': u"orm['organisation.Course']"}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '500', 'unique': 'True', 'null': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['content']
