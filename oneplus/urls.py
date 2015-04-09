944077c4fda56adb46a69fe4b610ef24f822fb95
msinovcic <msinovcic@retrorabbit.co.za>
2015-04-09 16:03:36 +0200

    Removed the view for demo

--- oneplus/urls.py
+++ oneplus/urls.py
@@ -62,5 +62,4 @@ urlpatterns = [
         name="prog.leader.id"),
     url(r"^badges$", views.badges, name="prog.badges"),
     url(r'^djga/', include('google_analytics.urls')),
-    url(r'^convert/', views.convert_mathml),
 ]
