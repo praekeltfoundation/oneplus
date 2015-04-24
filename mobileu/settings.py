645d7c546d2c6dddc5c062a1114f5b25e653e47e
dbreedt <dbreedt@retrorabbit.co.za>
2015-04-24 09:46:18 +0200

    Restored the sqlite db

--- mobileu/settings.py
+++ mobileu/settings.py
@@ -87,8 +87,8 @@ TEST_RUNNER = 'core.tests.TestRunner'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mobileu',
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
