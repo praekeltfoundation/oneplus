64e5fea02f2887522046e904d5da9b8175f17109
dbreedt <dbreedt@retrorabbit.co.za>
2015-05-07 12:26:06 +0200

    Added the mobileu urls

--- oneplusmvp/urls.py
+++ oneplusmvp/urls.py
@@ -11,7 +11,8 @@ urlpatterns = patterns(
     (r"^summernote/", include("django_summernote.urls")),
     url(r'^', include('oneplus.urls')),
     (r'^grappelli/', include('grappelli.urls')),
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^', include('mobileu.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
