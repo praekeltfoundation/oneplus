971edddac135525dfef6ff1098abcc0223695d8c
msinovcic <msinovcic@retrorabbit.co.za>
2015-05-14 09:58:20 +0200

    Changed how form checks for active participants and fixed errors

--- core/forms.py
+++ core/forms.py
@@ -7,15 +7,11 @@ class ParticipantCreationForm(forms.ModelForm):
     class Meta:
         model = Participant
 
-    def save(self, commit=True):
-        participant = super(ParticipantCreationForm, self).save(commit=False)
+    def clean_learner(self):
         learner = self.cleaned_data.get("learner")
         active_participant = Participant.objects.filter(learner=learner, is_active=True)
         if active_participant.exists():
             raise forms.ValidationError("This learner is currently an active participant in %s class. Learner cannot "
-                                        "be an active participant in multiple classes once."
-                                        % active_participant.first().classs__name)
-        else:
-            if commit:
-                participant.save()
-            return participant
+                                        "be an active participant in multiple classes at once."
+                                        % active_participant.first().classs.name)
+        return learner
