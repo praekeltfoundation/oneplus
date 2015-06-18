a3ea00cc44b858659c2523340f071ecad323bf45
hbarnardt <hbarnardt@retrorabbit.co.za>
2015-06-18 09:58:16 +0200

    Tests event awards points and badges

--- oneplus/tests.py
+++ oneplus/tests.py
@@ -698,8 +698,13 @@ class GeneralTests(TestCase):
 
         resp = self.client.get(reverse('learn.event_end_page'))
 
+        pbtr = ParticipantBadgeTemplateRel.objects.filter(badgetemplate=spot_test, scenario=badge,
+                                                   participant=self.participant)
+
         self.assertEquals(resp.status_code, 200)
         self.assertContains(resp, "Test End Page")
+        self.assertEquals(self.participant.points, 5)
+        self.assertIsNotNone(pbtr)
 
         event.name = "Exam"
         event.save()
