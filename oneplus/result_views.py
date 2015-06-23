4a5b4c270e0fbf7e8b317779903b027df0a1d364
dbreedt <dbreedt@retrorabbit.co.za>
2015-06-23 13:11:57 +0200

    Partial Class results

--- oneplus/result_views.py
+++ oneplus/result_views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.db.models import Count
-from organisation.models import Course, Module
-from core.models import ParticipantQuestionAnswer
 from django_boolean_sum import BooleanSum
+from organisation.models import Course, Module
+from core.models import ParticipantQuestionAnswer, Class, Learner
+from filters import get_timeframe_range
 
 
 class ResultsView(View):
@@ -14,7 +15,8 @@ class ResultsView(View):
     ]
 
     default_state = 0
-    default_timeframe = 4
+    default_timeframe = 3
+    default_module = 0
 
     def get_course(self, course):
         c = Course.objects.get(pk=course)
@@ -34,63 +36,217 @@ class ResultsView(View):
         return self.states[index]
 
     def get_module(self, module):
-        if module is None or module == "0":
-            return None
+        if module is None:
+            return self.default_module
 
-        return module
+        return int(module)
+
+    def get_modules(self, course, module):
+        modules = [{"id": 0, "name": "All"}]
+        modules += list(Module.objects.filter(coursemodulerel__course=course).values("id", "name"))
+
+        for temp_module in modules:
+            if temp_module["id"] == module:
+                temp_module["selected"] = True
+
+        return modules
+
+    def get_timeframe(self, timeframe):
+        if timeframe is None:
+            return self.default_timeframe
+
+        return int(timeframe)
 
     def get_timeframes(self, timeframe):
-        return []
+        timeframes = [
+            {"id": 0, "name": "All"},
+            {"id": 1, "name": "This week"},
+            {"id": 2, "name": "Last week"},
+            {"id": 3, "name": "This month"},
+            {"id": 4, "name": "Last month"},
+            {"id": 5, "name": "Last 3 months"},
+            {"id": 6, "name": "Last 6 months"},
+            {"id": 7, "name": "This year"},
+            {"id": 8, "name": "Last year"}
+        ]
 
-    def get(self, request, course):
+        for temp_timeframe in timeframes:
+            if temp_timeframe["id"] == timeframe:
+                temp_timeframe["selected"] = True
 
-        course_obj = self.get_course(course)
+        return timeframes
 
-        return render(
-            request,
-            "admin/results/results.html",
-            {
-                "course": course_obj,
-                "state": self.get_state(self.default_state)
-            }
-        )
+    def get_total_active_users(self, tf_start, tf_end):
+        learners = Learner.objects.all()
 
-    def post(self, request, course):
+        if tf_start and tf_end:
+            learners = learners.filter(last_active_date__range=[tf_start, tf_end])
 
+        return learners.aggregate(num_active=Count("pk"))["num_active"]
+
+    def get_total_inactive_users(self, tf_start, tf_end):
+        learners = Learner.objects.all()
+
+        if tf_start and tf_end:
+            learners = learners.exclude(last_active_date__range=[tf_start, tf_end])
+        else:
+            learners = learners.filter(last_active_date__isnull=True)
+
+        return learners.aggregate(num_inactive=Count("pk"))["num_inactive"]
+
+    def get_total_questions_core(self, answers, tf_start, tf_end):
+        if tf_start and tf_end:
+            answers = answers.filter(answerdate__range=[tf_start, tf_end])
+
+        ans = answers.aggregate(total=Count("pk"), correct=BooleanSum("correct"))
+
+        return ans["total"], ans["correct"], ans["total"] - ans["correct"]
+
+    def get_total_questions(self, tf_start, tf_end):
+        answers = ParticipantQuestionAnswer.objects.all()
+
+        return self.get_total_questions_core(answers, tf_start, tf_end)
+
+    def get_total_questions_per_module(self, tf_start, tf_end, module):
+        answers = ParticipantQuestionAnswer.objects.filter(testingquestion__module__pk=module)
+
+        return self.get_total_questions_core(answers, tf_start, tf_end)
+
+    def get_total_questions_per_class(self, tf_start, tf_end, cls):
+        answers = ParticipantQuestionAnswer.objects.filter(participant__class__pk=cls)
+
+        return self.get_total_questions_core(answers, tf_start, tf_end)
+
+    def get_total_questions_per_class_per_module(self, tf_start, tf_end, cls, module):
+        answers = ParticipantQuestionAnswer.objects.filter(participant__class__pk=cls, testingquestion__module__pk=module)
+
+        return self.get_total_questions_core(answers, tf_start, tf_end)
+
+    def get_data_dict(self, request, course):
         course_obj = self.get_course(course)
         state = self.get_state(self.default_state)
         module = None
+        module_filter = None
         modules = None
         module_results = None
-        timeframes = self.get_timeframes(self.default_timeframe)
+        timeframe = None
+        timeframes = None
+        timeframe_filter = None
+        tf_start = None
+        tf_end = None
+        class_results = None
 
         if "state" in request.POST.keys():
             state = self.get_state(request.POST["state"])
 
-        if "module" in request.POST.keys():
-            module = self.get_module(request.POST["module"])
+        if "module_filter" in request.POST.keys():
+            module_filter = request.POST["module_filter"]
+
+        if "timeframe_filter" in request.POST.keys():
+            timeframe_filter = request.POST["timeframe_filter"]
+
+        timeframe = self.get_timeframe(timeframe_filter)
+        timeframes = self.get_timeframes(timeframe)
+
+        if timeframe > 0:
+            tf_start, tf_end = get_timeframe_range(str(timeframe - 1))
 
         if state["id"] == 2:
-            modules = Module.objects.filter(coursemodulerel__course=course).values("id", "name")
             mr = ParticipantQuestionAnswer.objects.all()
 
+            module = self.get_module(module_filter)
+            modules = self.get_modules(course, module)
+
             if module:
-                mr.filter(question__module=module)
+                mr = mr.filter(question__module__pk=module)
 
-            module_results = mr.annotate(num_questions=Count("pk"))\
-                .annotate(num_correct=BooleanSum("correct"))\
-                .order_by("question__name")\
-                .values("question__name", "num_questions", "num_correct")
+            if tf_start and tf_end:
+                mr = mr.filter(answerdate__range=[tf_start, tf_end])
 
+            module_results = list(
+                mr.values("question__name")
+                .annotate(num_answers=Count("pk"))
+                .annotate(num_correct=BooleanSum("correct"))
+                .order_by("question__name")
+            )
+
+            for result in module_results:
+                if result["num_answers"] is not None and result["num_answers"] > 0:
+                    result["perc"] = result["num_correct"] * 100 / result["num_answers"]
+
+        if state["id"] == 3:
+            """
+            * Active Users
+                * Large Text: Shows total active users for time period selected.
+                * Small Text: Shows percentage of total users active
+            * Inactive Users
+                * Large Text: Shows total inactive users for time period selected.
+                * Small Text: Shows percentage of total users inactive for time period selected.
+            * Questions Answered
+                * Large Text: Shows total answers for time period selected.
+                * Small Text: Shows percentage of total questions answered for time period selected.
+            * Questions Correct
+                * Large Text: Shows percentage correct answers for time period selected
+                * Small Text: Shows total correct answers for time period selected.
+            * Questions Incorrect
+                * Large Text: : Shows percentage incorrect answers for time period selected.
+                * Small Text: Shows total incorrect answers for time period selected.
+            * Questions Correct per Module (Each module has an individual text display)
+                * Large Text: Shows percentage correct answers for time period selected
+                * Small Text: Shows total correct answers for time period selected.
+            """
+            classes = Class.objects.filter(course__pk=course)
+            modules = Module.objects.filter(coursemodulerel__course__pk=course)
+            class_results = []
+            class_module_results = []
+
+            for md in modules:
+                class_module_results.append({"large": 0, "small": 0, "name": md.name})
+
+            for cls in classes:
+                result = {
+                    "name": cls.name,
+                    "results": [
+                        {"large": 0, "small": 0, "name": "Active Users"},
+                        {"large": 0, "small": 0, "name": "Inactive Users"},
+                        {"large": 0, "small": 0, "name": "Questions Answered"},
+                        {"large": 0, "small": 0, "name": "Questions Correct"},
+                        {"large": 0, "small": 0, "name": "Questions Incorrect"},
+                        {"large": 0, "small": 0, "name": "Active Users"},
+                    ] + class_module_results
+                }
+
+                class_results.append(result)
+
+            total_active_learners = self.get_total_active_users(tf_start, tf_end)
+            total_inactive_learners = self.get_total_inactive_users(tf_start, tf_end)
+            total_questions, total_correct, total_incorrect = self.get_total_questions(tf_start, tf_end)
+
+        return {
+            "course": course_obj,
+            "state": state,
+            "modules": modules,
+            "module_results": module_results,
+            "timeframes": timeframes,
+            "class_results": class_results,
+        }
+
+    def get(self, request, course):
+
+        dd = self.get_data_dict(request, course)
 
         return render(
             request,
             "admin/results/results.html",
-            {
-                "course": course_obj,
-                "state": state,
-                "modules": modules,
-                "module_results": module_results,
-                "timeframes": timeframes
-            }
+            dd
+        )
+
+    def post(self, request, course):
+
+        dd = self.get_data_dict(request, course)
+
+        return render(
+            request,
+            "admin/results/results.html",
+            dd
         )
