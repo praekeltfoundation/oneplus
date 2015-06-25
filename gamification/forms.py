439c3202a62ce9f14d1a8f3a487e13a32707fdd0
dbreedt <dbreedt@retrorabbit.co.za>
2015-06-25 21:34:42 +0200

    Revert "Merge pull request #162 from praekelt/feature/goldenegg"
    This reverts commit ac7f30ea6dbc7259dfedbad9cc99ca6bafe62564, reversing
    changes made to d700242b0e96bfa7c46d180af8017288c3ae7053.

--- gamification/forms.py
+++ gamification/forms.py
@@ -10,13 +10,11 @@ class GamificationScenarioForm(forms.ModelForm):
         p = self.cleaned_data.get('point', None)
         if not p:
             raise ValidationError('A Scenario needs to be associated with a Point')
-        return p
 
     def clean_module(self):
         m = self.cleaned_data.get('module', None)
         if not m:
             raise ValidationError('A Scenario needs to be associated with a Module')
-        return m
 
     class Meta:
         model=GamificationScenario
