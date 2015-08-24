c18bf9efa8bb46ac50608373250d9d9b9b044a38
msinovcic <msinovcic@retrorabbit.co.za>
2015-08-24 10:05:13 +0200

    fixed pep error

--- oneplus/learn_views.py
+++ oneplus/learn_views.py
@@ -880,7 +880,7 @@ def sumit(request, state, user):
         _learnerstate = LearnerState(participant=_participant, sumit_level=1, sumit_question=1)
     elif not EventQuestionAnswer.objects.filter(event=_sumit, participant=_participant).exists():
         _learnerstate.sumit_level = 1
-        _learnerstate.sumit_question=1
+        _learnerstate.sumit_question = 1
         _learnerstate.save()
 
     request.session["state"]["next_tasks_today"] = \
