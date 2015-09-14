7fce91ca165b618b898ffb959ffdca67ba81c381
msinovcic <msinovcic@retrorabbit.co.za>
2015-09-14 12:43:46 +0200

    put tests back

--- oneplus/tests/test_learner_state_tests.py
+++ oneplus/tests/test_learner_state_tests.py
@@ -53,6 +53,43 @@ class LearnerStateTest(TestCase):
         return TestingQuestionOption.objects.create(
             name=name, question=question, correct=True)
 
+    def create_test_answer(
+            self,
+            participant,
+            question,
+            option_selected,
+            answerdate):
+        return ParticipantQuestionAnswer.objects.create(
+            participant=participant,
+            question=question,
+            option_selected=option_selected,
+            answerdate=answerdate,
+            correct=False
+        )
+
+    def create_and_answer_questions(self, num_questions, prefix, date):
+        answers = []
+        for x in range(0, num_questions):
+            # Create a question
+            question = self.create_test_question(
+                'q' + prefix + str(x), self.module)
+
+            question.save()
+            option = self.create_test_question_option(
+                'option_' + prefix + str(x),
+                question)
+            option.save()
+            answer = self.create_test_answer(
+                participant=self.participant,
+                question=question,
+                option_selected=option,
+                answerdate=date
+            )
+            answer.save()
+            answers.append(answer)
+
+        return answers
+
     def setUp(self):
         self.course = self.create_course()
         self.classs = self.create_class('class name', self.course)
