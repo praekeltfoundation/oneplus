e1a2be21f247e531e3532e8fd1ae72c703f49dfb
msinovcic <msinovcic@retrorabbit.co.za>
2015-10-12 15:01:46 +0200

    Locked down django-celery version

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     install_requires=[
         'Django==1.6.5',
         'South==0.8.4',
-        'celery==3.1.12',
+        'Celery==3.1.12',
         'django-grappelli==2.5.3',
         'django-summernote==0.6.8',
         'django-import-export==0.2.2',
-        'django-celery',
+        'django-celery==3.1.12',
         'Pillow==2.4.0',
         'psycopg2==2.5.3',
         'koremutake==1.0.5',
