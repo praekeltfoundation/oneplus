6c16ce37cba924ac7235c17890cb854587763458
msinovcic <msinovcic@retrorabbit.co.za>
2015-10-12 15:07:17 +0200

    fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'django-grappelli==2.5.3',
         'django-summernote==0.6.8',
         'django-import-export==0.2.2',
-        'django-celery==3.1.12',
+        'django-celery==3.1.16',
         'Pillow==2.4.0',
         'psycopg2==2.5.3',
         'koremutake==1.0.5',
