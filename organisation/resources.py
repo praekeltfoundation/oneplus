58018716ac2706db6518b083e72995cfa39d89b4
R. Jacobs <rjacobs@retrorabbit.co.za>
2017-01-26 12:35:23 +0200

    Customise school import.

--- organisation/resources.py
+++ organisation/resources.py
@@ -1,5 +1,5 @@
 from import_export import resources
-from organisation.models import School
+from organisation.models import Organisation, School
 
 
 class SchoolResource(resources.ModelResource):
@@ -37,3 +37,23 @@ class SchoolResource(resources.ModelResource):
             if school.open_type == ot[0]:
                 return ot[1]
         return ''
+
+    def import_obj(self, obj, data, dry_run):
+        if 'organisation' in data and data['organisation']:
+            try:
+                data['organisation'] = int(data['organisation'])
+            except:
+                try:
+                    data['organisation'] = Organisation.objects.get(name=data['organisation']).pk
+                except:
+                    raise
+
+        if 'open_type' in data and data['open_type']:
+            try:
+                data['open_type'] = int(data['open_type'])
+            except:
+                for ot in School.OPEN_TYPE_CHOICES:
+                    if data['open_type'] == ot[1]:
+                        data['open_type'] = ot[0]
+
+        return super(resources.ModelResource, self).import_obj(obj, data, dry_run)
