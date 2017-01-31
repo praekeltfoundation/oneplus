6a49f29c1344d64002ae2fcc238820f4f42194ee
R. Jacobs <rjacobs@retrorabbit.co.za>
2017-01-31 13:36:40 +0200

    Move Facebook metadata values to settings.

--- oneplus/context_processors.py
+++ oneplus/context_processors.py
@@ -3,4 +3,6 @@ def social_media(request):
     return {
         'FB_APP_NUM': settings.FB_APP_NUM,
         'FB_REDIRECT': settings.FB_REDIRECT,
+        'FB_SITE_TITLE': settings.FB_SITE_TITLE,
+        'FB_SITE_DESC': settings.FB_SITE_DESC,
     }
