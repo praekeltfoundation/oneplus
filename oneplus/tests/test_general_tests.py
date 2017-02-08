e75c69ea9520f80cc8bb4f0f9a9c5f8c55a8f5a5
R. Jacobs <rjacobs@retrorabbit.co.za>
2017-02-08 13:10:13 +0200

    refactored out the html formatting unit tests

--- oneplus/tests/test_general_tests.py
+++ oneplus/tests/test_general_tests.py
@@ -300,9 +300,6 @@ class GeneralTests(TestCase):
             resp = self.client.get(reverse('learn.event_end_page'))
             self.assertEquals(resp.status_code, 200)
 
-            # resp = self.client.get(reverse('learn.home'))
-            # self.assertContains(resp, "Points: <b>5</b>")
-
             for i in range(1, 15):
                 question = TestingQuestion.objects.create(name="Question %d" % i, module=self.module)
                 option = TestingQuestionOption.objects.create(name="Option %d.1" % i, question=question, correct=True)
@@ -643,57 +640,6 @@ class GeneralTests(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, "Link has been SMSed to you.")
 
-    def test_reset_password(self):
-        new_learner = create_learner(
-            self.school,
-            username="0701234567",
-            mobile="0701234567")
-
-        new_participant = create_participant(
-            new_learner,
-            self.classs,
-            datejoined=datetime.now())
-
-        resp = self.client.get(reverse('auth.reset_password', kwargs={'token': 'abc'}), follow=True)
-        self.assertRedirects(resp, '/onboarding')
-
-        new_learner.pass_reset_token = "abc"
-        new_learner.pass_reset_token_expiry = datetime.now() + timedelta(days=1)
-        new_learner.save()
-
-        resp = self.client.get(reverse('auth.reset_password', kwargs={'token': '%s' % new_learner.pass_reset_token}))
-        self.assertEquals(resp.status_code, 200)
-
-        #invalid form
-        resp = self.client.post(reverse('auth.reset_password', kwargs={'token': '%s' % new_learner.pass_reset_token}),
-                                data={})
-        self.assertContains(resp, "Please enter your new password")
-
-        #passwords not matching
-        resp = self.client.post(reverse('auth.reset_password', kwargs={'token': '%s' % new_learner.pass_reset_token}),
-                                data={
-                                    "password": '123',
-                                    "password_2": '23'
-                                })
-        self.assertContains(resp, "Passwords do not match")
-
-        password = "12345"
-        resp = self.client.post(reverse('auth.reset_password', kwargs={'token': '%s' % new_learner.pass_reset_token}),
-                                data={
-                                    "password": password,
-                                    "password_2": password
-                                })
-        self.assertContains(resp, "Password changed")
-
-        resp = self.client.post(
-            reverse('auth.login'),
-            data={
-                'username': new_learner.username,
-                'password': password},
-            follow=True
-        )
-        self.assertContains(resp, "WELCOME")
-
     def test_inbox_send(self):
         self.client.get(reverse('auth.autologin',
                                 kwargs={'token': self.learner.unique_token}))
@@ -729,100 +675,6 @@ class GeneralTests(TestCase):
 
         resp = self.client.post(reverse('com.chatgroups'))
         self.assertEquals(resp.status_code, 200)
-
-    def test_strip_p_tags(self):
-        content = "<p><b>Test</b></p>"
-        result = format_content(content)
-        self.assertEquals(result, "<div><b>Test</b></div>")
-
-    def test_align_image_only(self):
-        content = "<img/>"
-        result = format_option(content)
-        self.assertEquals(result, u'<img style="vertical-align:middle"/>')
-
-    def test_format_option_text_only(self):
-        content = "Test"
-        result = format_option(content)
-        self.assertEquals(result, u'Test')
-
-    def test_format_option_text_and_image(self):
-        content = "<b>Test</b><img/>"
-        result = format_option(content)
-        self.assertEquals(result, u'<b>Test</b><img style='
-                                  u'"vertical-align:middle"/>')
-
-    def test_format_option_double_image(self):
-        content = "<img/><img/>"
-        result = format_option(content)
-        self.assertEquals(result, u'<img style="vertical-align:middle"/>'
-                                  u'<img style="vertical-align:middle"/>')
-
-    def test_format_option(self):
-        content = "<b>Test</b><p></p><img/>"
-        output = format_option(content)
-        self.assertEquals(output, u'<b>Test</b><br/><img style="'
-                                  u'vertical-align:middle"/>')
-
-    def test_format_content(self):
-        content = '<img style="width:300px"/>'
-        result = format_content(content)
-        self.assertEquals(result, u'<div><img style="width:100%;'
-                                  u'vertical-align:middle"/></div>')
-
-    def test_already_format_content(self):
-        content = '<img style="width:100%"/>'
-        result = format_content(content)
-        self.assertEquals(result, u'<div><img style="width:100%;'
-                                  u'vertical-align:middle"/></div>')
-
-    def test_format_content_small_image(self):
-        content = '<img style="width:60px"/>'
-        result = format_content(content)
-        self.assertEquals(result, u'<div><img style="width:60px;'
-                                  u'vertical-align:middle"/></div>')
-
-    def test_filters_empty(self):
-        content = ""
-        output = format_content(content)
-        self.assertEquals(output, u'<div></div>')
-
-    def test_filters_empty_option(self):
-        content = ""
-        output = format_option(content)
-        self.assertEquals(output, u'')
-
-    def test_unicode_input(self):
-        content = u'Zoë'
-        output = format_option(content)
-        self.assertEquals(output, u'Zoë')
-
-    def test_save_then_display(self):
-        testingquestion = TestingQuestion.objects.create()
-        testingquestion.question_content = "There are 52 cards " \
-                                           "in a playing deck of cards. " \
-                                           "There are four Kings. " \
-                                           "If you draw out one card, " \
-                                           "the probability " \
-                                           "that it will be a King is: "
-        testingquestion.save()
-
-        self.assertEquals(testingquestion.question_content,
-                          u'<div>There are 52 cards '
-                          u'in a playing deck of cards. '
-                          u'There are four Kings. '
-                          u'If you draw out one card, '
-                          u'the probability '
-                          u'that it will be a King is: </div>')
-
-        content = format_content(testingquestion.question_content)
-
-        self.assertEquals(content,
-                          u'<div>There are 52 cards '
-                          u'in a playing deck of cards. '
-                          u'There are four Kings. '
-                          u'If you draw out one card, '
-                          u'the probability '
-                          u'that it will be a King is: </div>')
 
     def test_save_then_display2(self):
         testingquestionoption = TestingQuestionOption.objects.create(
--- oneplus/tests/test_html_formatting.py
+++ oneplus/tests/test_html_formatting.py
@@ -1,1 +1,101 @@
-__author__ = 'newuser'
+# -*- coding: utf-8 -*-
+from oneplus.templatetags.oneplus_extras import format_content, format_option
+from django.test import TestCase
+from content.models import TestingQuestion
+
+
+class TestHtmlFormatting(TestCase):
+
+    def test_strip_p_tags(self):
+        content = "<p><b>Test</b></p>"
+        result = format_content(content)
+        self.assertEquals(result, "<div><b>Test</b></div>")
+
+    def test_align_image_only(self):
+        content = "<img/>"
+        result = format_option(content)
+        self.assertEquals(result, u'<img style="vertical-align:middle"/>')
+
+    def test_format_option_text_only(self):
+        content = "Test"
+        result = format_option(content)
+        self.assertEquals(result, u'Test')
+
+    def test_format_option_text_and_image(self):
+        content = "<b>Test</b><img/>"
+        result = format_option(content)
+        self.assertEquals(result, u'<b>Test</b><img style='
+                                  u'"vertical-align:middle"/>')
+
+    def test_format_option_double_image(self):
+        content = "<img/><img/>"
+        result = format_option(content)
+        self.assertEquals(result, u'<img style="vertical-align:middle"/>'
+                                  u'<img style="vertical-align:middle"/>')
+
+    def test_format_option(self):
+        content = "<b>Test</b><p></p><img/>"
+        output = format_option(content)
+        self.assertEquals(output, u'<b>Test</b><br/><img style="'
+                                  u'vertical-align:middle"/>')
+
+    def test_format_content(self):
+        content = '<img style="width:300px"/>'
+        result = format_content(content)
+        self.assertEquals(result, u'<div><img style="width:100%;'
+                                  u'vertical-align:middle"/></div>')
+
+    def test_already_format_content(self):
+        content = '<img style="width:100%"/>'
+        result = format_content(content)
+        self.assertEquals(result, u'<div><img style="width:100%;'
+                                  u'vertical-align:middle"/></div>')
+
+    def test_format_content_small_image(self):
+        content = '<img style="width:60px"/>'
+        result = format_content(content)
+        self.assertEquals(result, u'<div><img style="width:60px;'
+                                  u'vertical-align:middle"/></div>')
+
+    def test_filters_empty(self):
+        content = ""
+        output = format_content(content)
+        self.assertEquals(output, u'<div></div>')
+
+    def test_filters_empty_option(self):
+        content = ""
+        output = format_option(content)
+        self.assertEquals(output, u'')
+
+    def test_unicode_input(self):
+        content = u'Zoë'
+        output = format_option(content)
+        self.assertEquals(output, u'Zoë')
+
+    def test_save_then_display(self):
+        testingquestion = TestingQuestion.objects.create()
+        testingquestion.question_content = "There are 52 cards " \
+                                           "in a playing deck of cards. " \
+                                           "There are four Kings. " \
+                                           "If you draw out one card, " \
+                                           "the probability " \
+                                           "that it will be a King is: "
+        testingquestion.save()
+
+        self.assertEquals(testingquestion.question_content,
+                          u'<div>There are 52 cards '
+                          u'in a playing deck of cards. '
+                          u'There are four Kings. '
+                          u'If you draw out one card, '
+                          u'the probability '
+                          u'that it will be a King is: </div>')
+
+        content = format_content(testingquestion.question_content)
+
+        self.assertEquals(content,
+                          u'<div>There are 52 cards '
+                          u'in a playing deck of cards. '
+                          u'There are four Kings. '
+                          u'If you draw out one card, '
+                          u'the probability '
+                          u'that it will be a King is: </div>')
