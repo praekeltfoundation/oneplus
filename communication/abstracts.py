53583e83077aca663b4e53cb6137be0487d9c82a
R. Jacobs <rjacobs@retrorabbit.co.za>
2017-02-20 13:55:38 +0200

    Add created/modified dates to comment likes.

--- communication/abstracts.py
+++ communication/abstracts.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
+from django.utils.translation import ugettext as _
 
 
 class CommentLikeAbstractModel(models.Model):
     """
-    An abstract base class that any custom comment models probably should
+    An abstract base class that any custom comment like models probably should
     subclass.
     """
 
+    date_created = models.DateTimeField(_('date created'), default=timezone.now)
+    date_updated = models.DateTimeField(_('date updated'), auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False, related_name="like_user")
     # comment -- Required field
 
