dff59ee9efaa31fd628d4bf5fae17ac5a521065c
R. Jacobs <rjacobs@retrorabbit.co.za>
2017-03-01 12:22:52 +0200

    Fix level badge migration dependencies.

--- oneplus/migrations/0005_assign_level_badge_urls.py
+++ oneplus/migrations/0005_assign_level_badge_urls.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(DataMigration):
 
+    depends_on = (
+        ("gamification", "0015_add_leveling_badge"),
+    )
+
     def forwards(self, orm):
         "Write your forwards methods here."
         # Note: Don't use "from appname.models import ModelName".
