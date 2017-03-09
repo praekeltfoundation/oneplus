562c10cfb6ad6a2083a7799ee141e7397d6c3dd9
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-03-09 15:26:17 +0200

    Add export resource for SMS.

--- communication/resources.py
+++ communication/resources.py
@@ -8,15 +8,54 @@ class SmsQueueResource(resources.ModelResource):
         model = SmsQueue
         fields = (
             'message',
-            'sent_date',
             'msisdn',
             'sent',
             'sent_date',
         )
         export_order = (
-            'message',
-            'sent_date',
             'msisdn',
             'sent',
             'sent_date',
+            'message',
         )
+
+    def dehydrate_sent_date(self, sms):
+        if sms.sent_date:
+            return str(sms.sent_date)
+        return ''
+
+
+class SmsResource(resources.ModelResource):
+
+    class Meta:
+        model = SmsQueue
+        fields = (
+            'message',
+            'msisdn',
+            'respond_date',
+            'responded',
+            'response',
+            'sent',
+            'date_sent',
+            'uuid',
+        )
+        export_order = (
+            'msisdn',
+            'uuid',
+            'sent',
+            'date_sent',
+            'message',
+            'responded',
+            'respond_date',
+            'response',
+        )
+
+    def dehydrate_date_sent(self, sms):
+        if sms.sent_date:
+            return str(sms.date_sent)
+        return ''
+
+    def dehydrate_respond_date(self, sms):
+        if sms.respond_date:
+            return str(sms.respond_date)
+        return ''
