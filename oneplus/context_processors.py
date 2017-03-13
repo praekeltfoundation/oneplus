7aed6ab2414442458387ed14d8a6374681b6517b
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-03-13 09:25:05 +0200

    Add setting for activating social media.

--- oneplus/context_processors.py
+++ oneplus/context_processors.py
@@ -1,6 +1,7 @@
 def social_media(request):
     from django.conf import settings
     return {
+        'SOCIAL_MEDIA_ACTIVE': settings.SOCIAL_MEDIA_ACTIVE,
         'FB_APP_NUM': settings.FB_APP_NUM,
         'FB_REDIRECT': settings.FB_REDIRECT,
         'FB_SITE_TITLE': settings.FB_SITE_TITLE,
