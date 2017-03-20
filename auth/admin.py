7541b124c4701b02d33d5965605149d4c937fe76
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-03-20 11:14:04 +0200

    Add Learner export action.

--- auth/admin.py
+++ auth/admin.py
@@ -20,6 +20,7 @@ from communication.tasks import bulk_send_all
 from communication.utils import VumiSmsApi
 from core.models import TeacherClass, ParticipantBadgeTemplateRel, Participant, ParticipantQuestionAnswer
 from gamification.models import GamificationScenario
+from mobileu.export import export_selected
 
 
 class SystemAdministratorAdmin(UserAdmin):
@@ -239,75 +240,6 @@ def send_message(modeladmin, request, queryset):
 send_message.short_description = "Send Message to selected learners"
 
 
-def delete_selected(modeladmin, request, queryset):
-    """
-    Default action which deletes the selected objects.
-
-    This action first displays a confirmation page whichs shows all the
-    deleteable objects, or, if the user has no permission one of the related
-    childs (foreignkeys), a "permission denied" message.
-
-    Next, it deletes all selected objects and redirects back to the change list.
-    """
-    opts = modeladmin.model._meta
-    app_label = opts.app_label
-
-    using = router.db_for_write(modeladmin.model)
-
-    # Populate deletable_objects, a data structure of all related objects that
-    # will also be deleted.
-    deletable_objects, perms_needed, protected = get_deleted_objects(
-        queryset, opts, request.user, modeladmin.admin_site, using)
-
-    # The user has already confirmed the deletion.
-    # Do the deletion and return a None to display the change list view again.
-    if request.POST.get('post'):
-        if perms_needed:
-            raise PermissionDenied
-        n = queryset.count()
-        if n:
-            for obj in queryset:
-                obj_display = force_text(obj)
-                modeladmin.log_deletion(request, obj, obj_display)
-            queryset.delete()
-            modeladmin.message_user(request, _("Successfully deleted %(count)d %(items)s.") % {
-                "count": n, "items": model_ngettext(modeladmin.opts, n)
-            }, messages.SUCCESS)
-        # Return None to display the change list page again.
-        return None
-
-    if len(queryset) == 1:
-        objects_name = force_text(opts.verbose_name)
-    else:
-        objects_name = force_text(opts.verbose_name_plural)
-
-    if perms_needed or protected:
-        title = _("Cannot delete %(name)s") % {"name": objects_name}
-    else:
-        title = _("Are you sure?")
-
-    context = {
-        "title": title,
-        "objects_name": objects_name,
-        "deletable_objects": [deletable_objects],
-        'queryset': queryset,
-        "perms_lacking": perms_needed,
-        "protected": protected,
-        "opts": opts,
-        "app_label": app_label,
-        'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
-    }
-
-    # Display the confirmation page
-    return TemplateResponse(request, modeladmin.delete_selected_confirmation_template or [
-        "admin/%s/%s/delete_selected_confirmation.html" % (app_label, opts.model_name),
-        "admin/%s/delete_selected_confirmation.html" % app_label,
-        "admin/delete_selected_confirmation.html"
-    ], context, current_app=modeladmin.admin_site.name)
-
-delete_selected.short_description = ugettext_lazy("Delete selected %(verbose_name_plural)s")
-
-
 class LearnerAdmin(UserAdmin, ImportExportModelAdmin):
     # The forms to add and change user instances
     form = LearnerChangeForm
@@ -351,7 +283,7 @@ class LearnerAdmin(UserAdmin, ImportExportModelAdmin):
         ("Enrollment", {"fields": ("enrolled",)}),
     )
 
-    actions = [send_sms, send_message]
+    actions = [send_sms, send_message, export_selected]
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         all_scenarios = GamificationScenario.objects.all()
