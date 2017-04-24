d0e7b8e62703e46f98f780796a159495dca0506a
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-04-24 14:13:42 +0200

    Refine ElasticSearch school methods.

--- mobileu/mobileu_elasticsearch.py
+++ mobileu/mobileu_elasticsearch.py
@@ -4,13 +4,9 @@ from elasticsearch import helpers as es_help, Elasticsearch
 from organisation.models import School
 
 
-es = Elasticsearch([settings.ELASTICSEARCH_URL])
-
-
 def ensure_indices():
     school_idx = SchoolIndex()
-    if not es.indices.exists(school_idx.index_name):
-        es.indices.create(school_idx.index_name)
+    school_idx.ensure_index()
 
 
 class ElasticSearchIndex:
@@ -18,6 +14,7 @@ class ElasticSearchIndex:
     index_name = None
 
     def __init__(self):
+        self.es = Elasticsearch([settings.ELASTICSEARCH_URL])
         if settings.ELASTICSEARCH_INDEX_PREFIX:
             self.index_name = settings.ELASTICSEARCH_INDEX_PREFIX + self.base_index_name
         else:
@@ -27,14 +24,25 @@ class ElasticSearchIndex:
         if not self.index_name:
             raise AssertionError('Index name not set.')
 
-        if not es.indices.exists(self.index_name):
-            es.indices.create(self.index_name)
+        if not self.es.indices.exists(self.index_name):
+            self.es.indices.create(self.index_name)
 
     def delete_index(self):
         if not self.index_name:
             raise AssertionError('Index name not set.')
 
-        es.indices.delete(self.index_name)
+        if not self.es.indices.exists(self.index_name):
+            self.es.indices.delete(self.index_name)
+
+    def all(self):
+        results = self.es.search(self.index_name, body={'query': {'match_all': {}}})
+        while type(results) == dict and results.get('hits', None):
+            results = results.pop('hits', None)
+        return results
+
+    def count(self):
+        count = self.es.count(index=self.index_name)
+        return count['count']
 
 
 class SchoolIndex(ElasticSearchIndex):
@@ -45,17 +53,18 @@ class SchoolIndex(ElasticSearchIndex):
                 '_id': school.pk,
                 '_index': self.index_name,
                 '_op_type': 'index',
+                '_type': 'document',
                 'date_updated': update_time,
                 'name': school.name,
                 'province': school.province,
             } for school in School.objects.all())
 
-        num_successful, errors = es_help.bulk(es, update_gen)
+        num_successful, errors = es_help.bulk(self.es, update_gen)
 
         if errors:
             pass
 
         if delete_stale:
-            es.delete_by_query(self.index_name, {'range': {'date_updated': {'lt': update_time}}})
+            self.es.delete_by_query(self.index_name, {'query': {'range': {'date_updated': {'lt': update_time}}}})
 
         return num_successful, errors
