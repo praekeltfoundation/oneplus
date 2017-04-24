1dc1c2dd1b4ffae4c4dd3237127531e680c97023
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-04-24 17:01:20 +0200

    Add tests for ElasticSearch fuzzy search.

--- mobileu/test_elasticsearch.py
+++ mobileu/test_elasticsearch.py
@@ -53,3 +53,35 @@ class TestSchoolIndex(TestCase):
         self.school_index.rebuild_index()
         sleep(1)
         self.assertEqual(self.school_index.count(), School.objects.count())
+
+    def test_search(self):
+        num_schools = 10
+        for i in xrange(num_schools):
+            create_school('School %i' % (i + 1,), self.organisation, province='Gauteng')
+        create_school('FREEDOM', self.organisation, province='Western Cape')
+        self.school_index.update_index()
+        sleep(1)
+
+        # test constrained name and province
+        results = self.school_index.search_name(search='School 1', province='Gauteng')
+        self.assertGreaterEqual(len(results), 1)
+        self.assertEqual(results[0]['_source']['name'], 'School 1')
+
+        results = self.school_index.search_name(search='freedome', province='Western Cape')
+        self.assertGreaterEqual(len(results), 1)
+        self.assertEqual(results[0]['_source']['name'], 'FREEDOM')
+
+        # test constrained name
+        results = self.school_index.search_name(search='School 1')
+        self.assertGreaterEqual(len(results), 1)
+        self.assertEqual(results[0]['_source']['name'], 'School 1')
+
+        results = self.school_index.search_name(search='School 1')
+        self.assertGreaterEqual(len(results), 1)
+        self.assertEqual(results[0]['_source']['name'], 'School 1')
+
+        # test constrained province
+        results = self.school_index.search_name(province='Western Cape')
+        self.assertGreaterEqual(len(results), 1)
+        self.assertEqual(results[0]['_source']['name'], 'FREEDOM')
+
