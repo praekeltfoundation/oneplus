f588752c23b6c0b72f655c9666ef44ff1f110253
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-04-24 14:11:40 +0200

    Remove Haystack from project.

--- mobileu/test_haystack.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.test import TestCase
-from haystack_custom import FuzzyBackend, FuzzyEngine, FuzzySearchNode, FuzzySearchQuery, FUZZY_VALID_FILTERS
-
-
-class TestHaystack(TestCase):
-    def test_haystack_barebones(self):
-        self.assertGreater(len(FUZZY_VALID_FILTERS), 0)
