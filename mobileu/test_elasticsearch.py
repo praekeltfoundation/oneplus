529146267e6498f21820c252780c216c882d652e
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-04-24 14:14:07 +0200

    Add tests for ElasticSearch school update.

--- mobileu/test_elasticsearch.py
+++ mobileu/test_elasticsearch.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from time import sleep
 from django.test import TestCase
 from django.test.utils import override_settings
 
@@ -17,7 +18,7 @@ def create_school(name, organisation, **kwargs):
         **kwargs)
 
 
-@override_settings(ELASTICSEARCH_INDEX_PREFIX='test_oneplus')
+@override_settings(ELASTICSEARCH_INDEX_PREFIX='test_oneplus_')
 class TestSchoolIndex(TestCase):
     def setUp(self):
         try:
@@ -25,6 +26,18 @@ class TestSchoolIndex(TestCase):
             self.school_index.ensure_index()
         except Exception as e:
             self.skipTest('ElasticSearch not available.')
+            return
+
+        self.organisation = create_organisation('Test Org')
 
     def tearDown(self):
         self.school_index.delete_index()
+
+    def test_update(self):
+        num_schools = 10
+        for i in xrange(num_schools):
+            create_school('School %i' % (i,), self.organisation, province='Gauteng')
+
+        self.school_index.update_index()
+        sleep(1)
+        self.assertEqual(self.school_index.count(), School.objects.count())
