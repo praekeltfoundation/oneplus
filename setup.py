4d6e845fc2a276db97731e2dda91998bb141afa0
JM Oberholzer <joberholzer@retrorabbit.co.za>
2018-01-10 14:03:25 +0200

    Changed psycopg2 from 2.5.3 to 2.7.3.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'go_http==0.1.1',
         'koremutake==1.0.5',
         'mock==1.0.1',
-        'psycopg2==2.5.3',
+        'psycopg2==2.7.3.2',
         'pyDNS',
         'requests==2.3.0',
         'responses==0.5.1',
